e6b6e3a454bd66948df97517e13ff399f6809f6b
sheeja5 <sheeja.stc@gmail.com>
2025-02-03 22:08:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if st.sidebar.button("Add Subject/Preference"):
 
 # Dropdown to select existing subject/preference
 selected_subject = st.sidebar.selectbox("Select a subject/preference:", list(notes.keys()))
-st.write("**Supporting Noty portal**")
+st.write("**Supporting Noty portal as well  by (abel and devansh)**")
 # Main area for adding and viewing notes
 if selected_subject:
     st.header(f"Notes for {selected_subject}")
